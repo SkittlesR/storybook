7830c74eaa90ac65c17ffbbb71e6ad49b895f65b
Elena the storyteller <skittlesrequiem@gmail.com>
2025-12-17 19:55:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,13 @@ window.onload = () => {
   document.querySelector(".spread").classList.add("visible");
 };
 
-// Звук и анимация бабочки при клике
+// Звук и лёгкая анимация при клике на бабочки
 const butterflies = document.querySelectorAll(".magic-butterfly");
-
 butterflies.forEach(butterfly => {
   const audio = new Audio('sounds/click.mp3');
 
   butterfly.addEventListener("click", () => {
-    // небольшой эффект увеличения
-    butterfly.style.transform = 'scale(1.2)';
+    butterfly.style.transform = 'scale(1.05)';
     audio.currentTime = 0;
     audio.play();
 
@@ -20,3 +18,4 @@ butterflies.forEach(butterfly => {
     }, 300);
   });
 });
+
