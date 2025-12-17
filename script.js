9b6b76954cc8e3c14a0fd9952695eb7a57321d2e
Elena the storyteller <skittlesrequiem@gmail.com>
2025-12-17 18:47:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 window.onload = () => {
   document.querySelector(".spread").classList.add("visible");
 };
+
+const scene = document.getElementById("scene");
+
+scene.addEventListener("click", () => {
+  scene.classList.add("active");
+
+  setTimeout(() => {
+    scene.classList.remove("active");
+  }, 300);
+});
